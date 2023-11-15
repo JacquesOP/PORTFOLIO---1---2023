703b03f0ab0a5e06acd026f961982fa5a19a458a
JacquesOP <jacquesoprince.pro@icloud.com>
2023-11-15 21:27:36 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,29 @@
+/*=============== SHOW MENU ===============*/
+
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+
+/*=============== SHADOW/BLUR HEADER ===============*/
+
+
+
+
+
+/*=============== SWIPER JS ===============*/
+
+
+
+/*===== GSAP ANIMATION =====*/
